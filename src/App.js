a0c07592be5ddd2acb2642b14551da4799799756
kushagra0333 <kushagrapandey0333@gmail.com>
2024-07-08 22:46:20 +0530

    remove a error

--- src/App.js
+++ src/App.js
@@ -1,21 +1,20 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import './App.css';
-import Home from "./component/home"
-import BlogPage from "./component/blogPage"
+import "./App.css";
+import Home from "./component/home";
+import BlogPage from "./component/blogPage";
 
 function App() {
   return (
     <div className="app">
       <Router basename="/blog">
-        
         <div className="main">
           <Routes>
             <Route index path="/" element={<Home />} />
             <Route path="/:slug" element={<BlogPage />} />
           </Routes>
         </div>
-   </Router>
+      </Router>
     </div>
   );
 }
